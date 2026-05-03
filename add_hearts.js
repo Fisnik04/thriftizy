8bf4f9f9161168ca5f17a07a7cc8bb9cb9f32af2
Fisnik04 <fisnikfazliu04@gmail.com>
2026-05-03 17:49:03 +0200

    Cleanup project: removed redundant scripts and temporary files

--- add_hearts.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const files = ['index.html', 'women.html', 'men.html', 'profile.html'];
-files.forEach(f => {
-  let content = fs.readFileSync(f, 'utf8');
-  content = content.replace(/<div class="product-img"/g, '<div class="heart-icon">🤍</div>\n                    <div class="product-img"');
-  fs.writeFileSync(f, content);
-  console.log('Updated ' + f);
-});
